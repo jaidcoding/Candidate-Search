02f9434834b67937d7430bbf01fb38799e209c62
Jaidon Clinton <jaidcoding@gmail.com>
2024-12-19 23:24:22 -0600

    fix: update base path in Vite config for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
-  base: '/<Candidate-Search>/', // Add this line
+  base: '/Candidate-Search/', // Add this line
 });
